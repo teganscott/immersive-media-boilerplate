b8c16c8a8b183e12c87f41226355787f7fe262ad
Ian Petrarca <ianpetrarca@users.noreply.github.com>
2020-04-28 12:03:28 -0400

    Transfer

---
+++ webpack/webpack.common.js
@@ -0,0 +1,58 @@
+const Path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.js')
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: 'js/[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/index.html'),
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|mov|otf|webp|svg|ttf|woff|woff2|wav|mp3|glb|gltf|glsl|opus)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            interpolate: true
+          }
+        }
+      }
+    ]
+  }
+};
